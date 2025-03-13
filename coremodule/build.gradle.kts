345be625aa7e5e1c9876a8356ec43a04bbcb0f2f
Vimalraj <vimalrajvjkr@gmail.com>
2025-03-13 23:32:57 +0530

    No Internet dialog Handling

--- coremodule/build.gradle.kts
+++ coremodule/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
     id("maven-publish")
 }
 
@@ -48,7 +49,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.Vimalraj-Vijay"
                 artifactId = "CoreModule"
-                version = "1.0.0"
+                version = "2.0.0"
             }
         }
     }
@@ -56,12 +57,22 @@ publishing {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.runtime.android)
-    implementation(libs.androidx.material3.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.lifecycle.runtime.ktx)
+    api(libs.androidx.activity.compose)
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.ui)
+    api(libs.androidx.ui.graphics)
+    api(libs.androidx.ui.tooling.preview)
+    api(libs.androidx.material3)
+    api(libs.androidx.runtime.livedata)
+    testApi(libs.junit)
+    androidTestApi(libs.androidx.junit)
+    androidTestApi(libs.androidx.espresso.core)
+    androidTestApi(platform(libs.androidx.compose.bom))
+    androidTestApi(libs.androidx.ui.test.junit4)
+    debugApi(libs.androidx.ui.tooling)
+    debugApi(libs.androidx.ui.test.manifest)
+    api(libs.androidx.lifecycle.viewmodel.compose)
+    api(libs.androidx.work.runtime.ktx)
 }
